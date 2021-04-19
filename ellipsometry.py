a3211e54b56a62ab6dda422c88b6d10562819bae
jmswgl <44481245+jmswgl@users.noreply.github.com>
2021-04-19 16:52:43 -0500

    Delete ellipsometry.py

--- ellipsometry.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import numpy as np
-import pandas as pd
-
-#df=pd.read_csv("/Users/jameswagle/Desktop/photoresponse project/Thin film_GaN/ellipsometry data/s1/50cycles_gan on s1 sample spec data.dat",sep="\t", skiprows=4)
-#df.head(5)
-print("hello")
